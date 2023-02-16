975266bd2249ccbd3705596a424af8995a78f372
Nazir Hussain <56648155+nazir-hussain@users.noreply.github.com>
2023-02-16 14:13:52 +0530

    Update MinimumElementInAnArray.java

--- IterativeStatement/MinimumElementInAnArray.java
+++ IterativeStatement/MinimumElementInAnArray.java
@@ -1,1 +1,18 @@
 
+package IterativeStatement;
+
+public class MinimumElementInAnArray {
+    public static void main(String[] args) {
+        int arr[] = {-5, -1, -2, -8, -9, -2, -10};
+
+        int minimum = arr[0];
+
+        for(int i = 1; i < arr.length; i++) {
+            if(arr[i] < minimum) {
+                minimum = arr[i];
+            }
+        }
+
+        System.out.println(minimum);
+    }
+}
